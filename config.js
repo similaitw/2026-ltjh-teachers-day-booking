e1675dc2a1aa56ba01ef0a7afd51534d16a0db48
similai.tw <similai.tw@gmail.com>
2026-09-13 17:18:55 +0800

    Add booking app configuration

---
+++ config.js
@@ -0,0 +1,8 @@
+window.BOOKING_CONFIG = {
+  eventId: "ltjh-teachers-day-2026",
+  capacityPerSlot: 8,
+  eventDate: "2026-09-16",
+  eventLabel: "9/16（三）",
+  eventTime: "13:00–16:00",
+  venue: "大表藝教室"
+};
